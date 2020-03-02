ab26d7301d6b8e458f3608d48fc6179383c270d4
jhamil-conaca-coderoad-com <jhamil.conaca@mojix.com>
2020-03-02 17:23:47 -0400

    Add ci to user

--- src/staticFactory/User.java
+++ src/staticFactory/User.java
@@ -3,14 +3,16 @@ package staticFactory;
 public class User {
     private String name;
     private String lastName;
+    private String ci;
 
     private User() {
     }
 
-    public static User from(String name , String latName) {
+    public static User from(final String name , final String latName, final String ci) {
         User user = new User();
         user.name = name;
         user.lastName = latName;
+        user.ci = ci;
         return user;
     }
 }
