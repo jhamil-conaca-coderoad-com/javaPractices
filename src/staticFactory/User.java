ac62b201c2b128daf398bb8dc4d38061290a4c20
jhamil-conaca-coderoad-com <jhamil.conaca@mojix.com>
2020-03-02 17:25:12 -0400

    Add phone param to user

--- src/staticFactory/User.java
+++ src/staticFactory/User.java
@@ -3,14 +3,16 @@ package staticFactory;
 public class User {
     private String name;
     private String lastName;
+    private String phone;
 
     private User() {
     }
 
-    public static User from(String name , String latName) {
+    public static User from(String name , String latName, String phone) {
         User user = new User();
         user.name = name;
         user.lastName = latName;
+        user.phone = phone;
         return user;
     }
 }
